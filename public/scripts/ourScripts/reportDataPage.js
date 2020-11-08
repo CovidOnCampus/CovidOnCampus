66f82c9e5036a99f64ea4f39a35a6b0907bd1731
chaplim1 <chaplim1@rose-hulman.edu>
2020-11-08 09:56:24 -0500

    working monitor listener

--- public/scripts/ourScripts/reportDataPage.js
+++ public/scripts/ourScripts/reportDataPage.js
@@ -37,6 +37,44 @@ rhit.FbReportDataPageManager = class {
 
 
 
+
+
+
+//added
+
+document.query("cough").value = cough;
+document.query("difficultyBreathing").value = cough;
+document.query("chills").value = cough;
+document.query("musclePain").value = cough;
+document.query("headache").value = cough;
+document.query("soreThroat").value = cough;
+document.query("tasteOrSmell").value = cough;
+
+
+rhit.FbMapPageManager = class {
+	constructor(uid) {
+		this._uid = uid;
+		this._documentSnapshots = [];
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_LOCATIONS);
+		this._unsubscribe = null;
+	}
+
+
+	// beginListening(changeListener) {
+	// 	let query = this._ref.orderBy(rhit.FB_KEY_RATING).limit(50);
+
+	// 	this._unsubscribe = query.onSnapshot((querySnapshot) => {
+	// 		this._documentSnapshots = querySnapshot.docs;
+	// 		changeListener();
+    // 	});
+	// }
+	// stopListening() {
+	// 	this._unsubscribe();
+	// }
+
+};
+
+
 //use 146 for reference with the manager
 
 //doc.query("#idForEachSymptom").value = CONSTANT (make a const for each symptom)
