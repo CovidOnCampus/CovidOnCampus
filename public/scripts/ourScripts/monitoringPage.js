bda2856bd5232d6ce65466c625b650fadeb13260
jurgenkr <jurgenkr@rose-hulman.edu>
2020-11-13 20:58:08 -0500

    remove extra code

--- public/scripts/ourScripts/monitoringPage.js
+++ public/scripts/ourScripts/monitoringPage.js
@@ -6,20 +6,6 @@ rhit.MonitoringPageController = class {
 
 		document.querySelector("#reportData").onclick = (event) => {
 			window.location.href = `/reportDataPage.html`;
-			firebase.firestore().collection(rhit.FB_COLLECTION_MONITORING).doc(rhit.fbReviewsPageManager.locId).get().then(function(doc) {
-				if (doc.exists) {
-					let building = doc.get(rhit.FB_KEY_BUILDING);
-					let description = doc.get(rhit.FB_KEY_DESCRIPTION);
-					let type = doc.get(rhit.FB_KEY_TYPE);
-					let rating = doc.get(rhit.FB_KEY_RATING);
-					document.querySelector("#reviewsTitle").innerHTML = `<h3>${building}</h3><h5>Reviews for ${description} ${type}</h5>
-																			<h6>Current rating is ${rating.toFixed(2)}`;
-				} else {
-					console.log("No such document!");
-				}
-			}).catch(function(error) {
-				console.log("Error getting document:", error);
-			});
 		}
 		document.querySelector("#recentData").onclick = (event) => {
 			window.location.href = `/recentDataPage.html`;
